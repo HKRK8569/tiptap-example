db98196c72e02c4d952ee5ac3c9534fd0de49c22
HKRK8569 <kayann3021@gmail.com>
2025-01-30 11:40:45 +0900

    自作のimageに変更

--- src/components/Tiptap/index.tsx
+++ src/components/Tiptap/index.tsx
@@ -1,15 +1,20 @@
 import Placeholder from "@tiptap/extension-placeholder";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { Image } from "../../extensions/ImageExtension";
 
 const Editor = () => {
+  const content = `<figure class="e-image ProseMirror-selectednode" data-natural-width="500" data-natural-height="500" data-size="default" data-style="default" contenteditable="false" draggable="true"><img src="500x500.png" width="500" height="500" alt=""></figure>`;
+
   const editor = useEditor({
     extensions: [
       StarterKit.configure({ heading: { levels: [2, 3, 4] } }),
       Placeholder.configure({
         placeholder: "文字を入力してください。",
       }),
+      Image,
     ],
+    content,
     editorProps: {
       attributes: {
         class: "p-4 focus:outline-none h-full w-full",
